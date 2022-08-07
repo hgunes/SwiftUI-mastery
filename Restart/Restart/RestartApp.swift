48fff1cb8c604207bd9caf9b2dd2ec3bbb820ba8
hgunes <h.gunes13@gmail.com>
2022-08-07 17:10:30 +0200

    new: Restart

---
+++ Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Harun Gunes on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
