63f5c10896809e0240d3777ee7aee105a59d3b5d
hgunes <h.gunes13@gmail.com>
2022-08-07 22:24:32 +0200

    new: LOTR-Converter

---
+++ LOTR-Converter/LOTR-Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR-Converter
+//
+//  Created by Harun Gunes on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
